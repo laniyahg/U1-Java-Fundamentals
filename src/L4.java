03b22068081d9b1bb14bdbed6d6cf1b2c1039bd7
glasslan001 <glasslan001@warren.k12.in.us>
2021-08-09 13:04:31 -0400

    completed 1.3 - 5 notes

--- src/L4.java
+++ src/L4.java
@@ -1,2 +1,31 @@
 public class L4 {
+
+    static class Assignments {
+        public static void main(String[] args) {
+
+            int value = 5;
+
+
+
+
+            //Declare a variable - stating the datatype and identifier
+
+            double total;
+            String message = "Hi";
+
+            // Assign value
+
+            total = 6;
+
+            System.out.println(message + ", the value is " + total);
+
+            total = 10;
+            message = "Bye";
+            //GRAVITY = -10;   -- dis is bad
+
+            System.out.println(message + ", the value is " + total);
+
+
+        }
+    }
 }
--- src/L5.java
+++ src/L5.java
@@ -1,2 +1,29 @@
 public class L5 {
+
+    public static void main(String[] args){
+
+       /*double basePay = 7.25;
+        double hoursWeek1 = 16.5;
+        double hoursWeek2 = 23.75;
+
+        double totalHours = hoursWeek1 + hoursWeek2;
+        double wages = basePay * totalHours;
+
+        System.out.println("Your bi-weekly paycheck is: $" + wages);*/
+
+        double fraction = 1/2.0;
+        System.out.println(fraction);
+    }
 }
+        //25 % 6 = 1
+        //31 % 5 = 1
+        //17 % 3 = 2
+        // 4 %  2 = 0
+        // 5 % 2 = 1
+        // 6 % 2 = 0
+        // 121 % 100 = 21
+        //55 % 15 = 10
+
+
+
+
